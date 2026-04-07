dbe5dfbbc108533d55c503ffa86dd013e062f06e
Devscar <notechies1@mail.ru>
2026-04-07 19:15:46 +0300

    переписал функции и сделал новые, поправил модель, чтобы она нормально принимала дату,

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"time"
 	"warehouse/config"
 	"warehouse/logger"
@@ -26,7 +25,7 @@ func main() {
 		panic(err)
 	}
 	ctx := context.Background()
-/*
+
 	testRepo := repository.NewWHouseRepository(sqlDB)
 	now := time.Now()
 	test := models.ProductModel{
@@ -57,49 +56,48 @@ func main() {
 		panic(err)
 	}
 	/*
-		err = testRepo.ProductDelete(ctx, test.Article)
-		if err != nil {
-			log.Error("failed to delete product from database, fatal error ", err)
-			panic(err)
-		}
-
-		test.Article = 2
-		test.ProductName = "Test product 2"
-		test.DeliveryDate = time.Now()
-		test.ExpireDate = time.Now()
-		test.Weight = 1000
-		/*
-			err = testRepo.ProductADD(ctx, test)
+			err = testRepo.ProductDelete(ctx, test.Article)
 			if err != nil {
-				log.Error("failed to add product to database, fatal error ", err)
+				log.Error("failed to delete product from database, fatal error ", err)
+				panic(err)
 			}
 
+			test.Article = 2
+			test.ProductName = "Test product 2"
+			test.DeliveryDate = time.Now()
+			test.ExpireDate = time.Now()
+			test.Weight = 1000
+			/*
+				err = testRepo.ProductADD(ctx, test)
+				if err != nil {
+					log.Error("failed to add product to database, fatal error ", err)
+				}
 
-	testProducts, err := testRepo.ListProducts(ctx)
-	if err != nil {
-		log.Error("failed to list products from database, fatal error ", err)
-		panic(err)
-	}
-	for _, product := range testProducts {
-		fmt.Println(product)
-	}
 
-	TestStorages, err := testRepo.ListStorages(ctx)
-	if err != nil {
-		log.Error("failed to list storages from database, fatal error ", err)
-		panic(err)
-	}
-	for _, storage := range TestStorages {
-		fmt.Println(storage)
-	}
+		testProducts, err := testRepo.ListProducts(ctx)
+		if err != nil {
+			log.Error("failed to list products from database, fatal error ", err)
+			panic(err)
+		}
+		for _, product := range testProducts {
+			fmt.Println(product)
+		}
 
-	TestProduct, err := testRepo.GetProduct(ctx, testProducts[0].Article)
-	if err != nil {
-		log.Error("failed to get product from database, fatal error ", err)
-		panic(err)
-	}
-	fmt.Println(TestProduct)
-*/
- */
+		TestStorages, err := testRepo.ListStorages(ctx)
+		if err != nil {
+			log.Error("failed to list storages from database, fatal error ", err)
+			panic(err)
+		}
+		for _, storage := range TestStorages {
+			fmt.Println(storage)
+		}
+
+		TestProduct, err := testRepo.GetProduct(ctx, testProducts[0].Article)
+		if err != nil {
+			log.Error("failed to get product from database, fatal error ", err)
+			panic(err)
+		}
+		fmt.Println(TestProduct)
+	*/
 
 }
